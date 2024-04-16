6114c87df28735430e0acb12b939a3ebc7317ec8
benjamin <benjaminlikita3@gmail.com>
2024-04-16 01:20:08 +0100

    updated app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,6 +9,7 @@ import { homedir } from 'os';
 export class AppService {
   constructor(private prisma: PrismaService) {}
   getHello(): string {
+    console.log(join(homedir(), __dirname, "src/uploads"))
     return 'Hello World!';
   }
 
@@ -16,8 +17,8 @@ export class AppService {
     
     const date = new Date()
     const datePath = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-    const dirExists = existsSync(join(homedir(), `src/uploads/${datePath}`))
-    if(!dirExists) mkdirSync(join(homedir(), `src/uploads/${datePath}`))
+    const dirExists = existsSync(join(homedir(), __dirname, `src/uploads/${datePath}`))
+    if(!dirExists) mkdirSync(join(homedir(), __dirname, `src/uploads/${datePath}`))
 
       console.log(puppeteer.executablePath())
     const browser = await puppeteer.launch({
@@ -48,7 +49,7 @@ export class AppService {
       })
       
       const imageName = `${date.getTime()}.jpg`
-      await page.screenshot({path: resolve(`./src/uploads/${datePath}/${imageName}`)})
+      await page.screenshot({path: join(homedir(), __dirname, `src/uploads/${datePath}/${imageName}`)})
 
       
       
